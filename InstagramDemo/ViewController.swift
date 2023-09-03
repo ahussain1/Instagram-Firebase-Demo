4bf0d2b768a4fda66f422fc183b6f11c5525065a
Arif Hussain <hussain.arif33@gmail.com>
2023-09-03 11:34:03 -0400

    Initial Commit

---
+++ InstagramDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramDemo
+//
+//  Created by Arif  on 9/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
